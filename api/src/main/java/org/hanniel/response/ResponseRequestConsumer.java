30e2e81d874512afe27c41eb6b408623060eb905
Hanniel Vieira <hannielvieira1227@gmail.com>
2024-01-31 17:24:04 -0300

    feat: create ResponseRequestConsumer

--- api/src/main/java/org/hanniel/response/ResponseRequestConsumer.java
+++ api/src/main/java/org/hanniel/response/ResponseRequestConsumer.java
@@ -1,4 +1,21 @@
 package org.hanniel.response;
 
+import org.hanniel.Facade.ResponseFacade;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ResponseRequestConsumer {
+
+    @Autowired
+    ResponseFacade responseFacade;
+
+    @RabbitListener(queues = {"response_success_queue"})
+    public void receive(@Payload Message message){
+        String payload = String.valueOf(message.getPayload());
+        responseFacade.responseStatus(payload);
+    }
 }
