0d6562b08a02fc91e07fd0098dcb9670f35b300d
Hanniel Vieira <hannielvieira1227@gmail.com>
2024-01-31 17:45:47 -0300

    feat: create ResponseDTO

---
+++ api/src/main/java/org/hanniel/dto/ResponseDTO.java
@@ -0,0 +1,22 @@
+package org.hanniel.dto;
+
+public class ResponseDTO {
+    private String name;
+    private String response;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
